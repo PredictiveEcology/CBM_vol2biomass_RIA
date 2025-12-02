3af037c20d6babe678c3d3774cfdf870c50873c9
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-12-02 15:11:29 -0800

    change to use latin_full column instead of NFI

--- CBM_vol2biomass_RIA.R
+++ CBM_vol2biomass_RIA.R
@@ -210,10 +210,12 @@ Init <- function(sim) {
   if (any(!c("canfi_species", "genus", "sw_hw") %in% names(sim$userGcMeta))){
 
     sppMatchTable <- CBMutils::sppMatch(
-      sim$userGcMeta$species, return = c("CanfiCode", "NFI", "Broadleaf"))[, .(
+      sim$userGcMeta$species, return = c("CanfiCode", "Latin_full", "Broadleaf"))[, .(
         canfi_species = CanfiCode,
         sw_hw         = data.table::fifelse(Broadleaf, "hw", "sw"),
-        genus         = sapply(strsplit(NFI, "_"), `[[`, 1)
+        genus         = toupper(
+          sapply(strsplit(latinFull, ""), function(x) paste(x[1:4], collapse = ""))
+        )
       )]
 
     sim$userGcMeta <- cbind(
